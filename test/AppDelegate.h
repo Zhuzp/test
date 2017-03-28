f4dadfcd55e805fdbca2366e0b97c53e1170f480
Zhuzp <1964418121>
2017-03-28 21:37:25 +0800

    提交代码

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 朱正品 on 17/3/28.
+//  Copyright © 2017年 zzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
